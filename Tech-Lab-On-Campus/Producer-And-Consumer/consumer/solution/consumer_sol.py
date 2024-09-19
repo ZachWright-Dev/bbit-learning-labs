f8e7f4eadc7010721fcc77fe0967855ba9a1f7d6
Nathan Chery <nathanchery181@gmail.com>
2024-09-19 19:03:53 -0400

    Commit

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,64 @@
+import pika
+import os
+
+from consumer_interface import mqConsumerInterface
+con_params = pika.URLParameters(os.environ["AMQP_URL"])
+connection = pika.BlockingConnection(parameters=con_params)
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(self, binding_key, exchange_name, queue_name):
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.q_name = queue_name
+
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        # Set-up Connection to RabbitMQ service
+
+        # Establish Channel
+        self.channel = connection.channel()
+
+        # Create Queue if not already present
+        self.channel.queue_declare(queue="Queue Name")
+
+        # Create the exchange if not already present
+        self.channel.exchange_declare(
+            exchange="Exchange Name", exchange_type="topic"
+        )
+
+        # Bind Binding Key to Queue on the exchange
+        self.channel.queue_bind(
+            queue= "Queue Name",
+            routing_key= "Routing Key",
+            exchange="Exchange Name",
+        )
+
+        # Set-up Callback function for receiving messages
+        self.channel.basic_consume("Queue Name", Function Name, auto_ack=False)
+
+    def on_message_callback(self, channel, method_frame, header_frame, body) -> None:
+        # Acknowledge message
+        self.channel.basic_ack(method_frame.delivery_tag, False)
+
+        #Print message (The message is contained in the body parameter variable)
+
+        print(body)
+
+    def startConsuming(self) -> None:
+        # Print " [*] Waiting for messages. To exit press CTRL+C"
+        print("[*] Waiting for messages. To exit press CTRL+C")
+
+        # Start consuming messages
+        self.channel.start_consuming()
+    
+    def __del__(self) -> None:
+        # Print "Closing RMQ connection on destruction"
+        print("Closing RMQ connection on destruction")
+        
+        # Close Channel
+        self.channel.close()
+
+        # Close Connection
+        connection.close()
+ 
